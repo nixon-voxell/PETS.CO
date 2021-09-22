8627d4c3484680a19e2001f7de923af976066cce
Nixon <voxell.technologies@gmail.com>
2021-09-22 18:33:50 +0800

    create table taking sql command

--- includes/init.inc.php
+++ includes/init.inc.php
@@ -3,22 +3,5 @@
 require_once "utils/dbhandler.php";
 require_once "utils/common_util.php";
 
-function CreateMemberTable($conn)
-{
-  $sql = "CREATE TABLE IF NOT EXISTS account (
-    id int(100) PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    username varchar(50) NOT NULL,
-    password varchar(512) NOT NULL,
-    email varchar(50) NOT NULL
-    );";
-
-  $stmt = mysqli_stmt_init($conn);
-  if (!mysqli_stmt_prepare($stmt, $sql))
-    echo("ERROR");
-
-  mysqli_stmt_execute($stmt);
-  mysqli_stmt_close($stmt);
-  exit();
-}
-
-CreateMemberTable($conn);
+CreateNeededTables($conn);
+exit();
--- includes/utils/init_util.php
+++ includes/utils/init_util.php
@@ -1,4 +1,24 @@
 <?php
 // create all needed tables
-function CreateNeededTables()
-{}
+function CreateNeededTables($conn)
+{
+  // account table
+  $sql = "CREATE TABLE IF NOT EXISTS account (
+    id int(100) PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    username varchar(50) NOT NULL,
+    password varchar(512) NOT NULL,
+    email varchar(50) NOT NULL
+    );";
+
+  CreateTable($conn, $sql);
+}
+
+function CreateTable($conn, $sql)
+{
+  $stmt = mysqli_stmt_init($conn);
+  if (!mysqli_stmt_prepare($stmt, $sql))
+    echo("ERROR");
+
+  mysqli_stmt_execute($stmt);
+  mysqli_stmt_close($stmt);
+}
