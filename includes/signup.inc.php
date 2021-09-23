310ac8cd11630fe56a843c54bacfb95d52503df1
andrewtch88 <andrewtch91@gmail.com>
2021-09-23 22:25:19 +0800

    Fixed empty input can redirect to login bug

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -10,7 +10,10 @@ if (isset($_POST["submit"]))
   require_once "utils/signup_util.php";
 
   if(EmptyInputSignup($username, $pwd, $repeatPwd, $email) !== false)
+  {
     header("location: ../signup.php?error=emptyinput");
+    exit();
+  }
   else if(InvalidUid($username) !== false)
     header("location: ../signup.php?error=invaliduid");
   else if(PwdMatch($pwd, $repeatPwd) !== false)
