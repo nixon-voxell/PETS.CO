0d57b6dac1b4fb891405c185171afdb727f12037
Nixon <voxell.technologies@gmail.com>
2021-10-16 14:24:47 +0800

    manage profile page is now visible

--- manage_profile.php
+++ manage_profile.php
@@ -6,46 +6,51 @@
 <div class="container">
   <h3 class="grey-text">Manage Personal Profile</h3>
   <button id="edit" class="btn orange" onclick="edit_profile(this)">Edit</button>
-  <form class="col s12" action="includes/manage_profile.inc.php" method="post">
-    <div class="row">
-      <div class="input-field col s6">
-        <i class="material-icons prefix">account_circle</i>
-        <?php
-        echo "<input name='id' type='hidden' value='$memberID'/>";
-        echo"<input name='username' type='text' value='$username'/>";
-        ?>
-        <label for="username">Enter New Username</label>
-        <span class="helper-text" data-error="Min 5, Max 12 characters" data-success="correct">Min 5, Max 12 characters</span>
-      </div>
+
+  <div class="card white" style="box-shadow: black 2px 10px 20px 10px;">
+    <div class="card-content">
+      <form class="col s12" action="includes/manage_profile.inc.php" method="post">
+        <div class="row">
+          <div class="input-field col s6">
+            <i class="material-icons prefix">account_circle</i>
+            <?php
+            echo "<input name='id' type='hidden' value='$memberID'/>";
+            echo"<input name='username' type='text' value='$username'/>";
+            ?>
+            <label for="username">Enter New Username</label>
+            <span class="helper-text" data-error="Min 5, Max 12 characters" data-success="correct">Min 5, Max 12 characters</span>
+          </div>
+        </div>
+        <div class="row">
+          <div class="input-field col s6">
+            <i class="material-icons prefix">email</i>
+            <?php
+            echo "<input name='email' type='text' value='$email'/>";
+            ?>
+            <label for="email">Enter New Email</label>
+            <span class="helper-text" data-error="wrong" data-success="correct"></span>
+          </div>
+        </div>
+        <div class="row" id="enter_pwd">
+          <div class="input-field col s6">
+            <i class="material-icons prefix"> password</i>
+            <input name="pwd" type="password" class="validate" minlength="6" maxlength="20">
+            <label for="password">Enter New Password</label>
+            <span class="helper-text" data-error="Min 8, Max 20 characters" data-success="correct">Min 8, Max 20 characters</span>
+          </div>
+        </div>
+        <div class="row" id="enter_repeat_pwd">
+          <div class="input-field col s6">
+            <i class="material-icons prefix"> password</i>
+            <input name="repeatPwd" type="password" class="validate" maxlength="14">
+            <label for="repeatPwd"> Repeat New Password</label>
+          </div>
+        </div>
+        <br>
+        <button type="submit" name="update" class="btn">Update Account</button>
+      </form>
     </div>
-    <div class="row">
-      <div class="input-field col s6">
-        <i class="material-icons prefix">email</i>
-        <?php
-        echo "<input name='email' type='text' value='$email'/>";
-        ?>
-        <label for="email">Enter New Email</label>
-        <span class="helper-text" data-error="wrong" data-success="correct"></span>
-      </div>
-    </div>
-    <div class="row" id="enter_pwd">
-      <div class="input-field col s6">
-        <i class="material-icons prefix"> password</i>
-        <input name="pwd" type="password" class="validate" minlength="6" maxlength="20">
-        <label for="password">Enter New Password</label>
-        <span class="helper-text" data-error="Min 8, Max 20 characters" data-success="correct">Min 8, Max 20 characters</span>
-      </div>
-    </div>
-    <div class="row" id="enter_repeat_pwd">
-      <div class="input-field col s6">
-        <i class="material-icons prefix"> password</i>
-        <input name="repeatPwd" type="password" class="validate" maxlength="14">
-        <label for="repeatPwd"> Repeat New Password</label>
-      </div>
-    </div>
-    <br>
-    <button type="submit" name="update" class="btn">Update Account</button>
-  </form>
+  </div>
 
   <div class="errormsg">
     <?php
