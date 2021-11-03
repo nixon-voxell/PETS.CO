18230c76017d8adce1b1ffb7903d9126204038a5
Nixon <voxell.technologies@gmail.com>
2021-11-03 10:57:00 +0800

    basic search catalog page

--- order_items.php
+++ order_items.php
@@ -1,5 +1,15 @@
 <?php
+  require_once "includes/utils/dbhandler.php";
+  if (isset($_GET["member_id"]))
+  {
+    /** @var int $memberID */
+    $memberID = $_GET["member_id"];
+    $member = Member::CreateMemberFromID($memberID, $conn);
+    $orders = $member->GetOrders();
+    $orderCount = count($orders);
+  }
 ?>
+
 <h4 class="white-text page-title">Previous Orders</h4>
 <h5 class="white-text">#1</h5>
 <div class="row">
---
+++ search_catalog.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<title>PETS.CO</title>
+<?php include "header.php"; ?>
+
+<div class="container" style="padding-top: 50px;">
+  <div class="rounded-card-parent">
+    <nav class="nav-wrapper rounded-card">
+    </nav>
+  </div>
+</div>
+
+<?php include "footer.php"; ?>
+</html>
