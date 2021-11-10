678e32182cf15cc1bc010d5be0a6087e1f98b902
Nixon <voxell.technologies@gmail.com>
2021-11-10 11:37:59 +0800

    remove on mouse hover javascript, moved value images to the right folder

--- static/js/footer.js
+++ static/js/footer.js
@@ -9,21 +9,3 @@ $('.carousel.carousel-slider').carousel({
   fullWidth: true,
   indicators: true
 });
-
-var selectableCards = document.getElementsByClassName('selectable-card');
-
-if (selectableCards != null)
-{
-  for (var c=0; c < selectableCards.length; c++)
-  {
-    element = selectableCards[c];
-    element.addEventListener("mouseover", function(event)
-    {
-      event.target.classList.add("selectable-card-transition");
-      setTimeout(() =>
-      {
-        event.target.classList.remove("selectable-card-transition");
-      }, 400);
-    });
-  }
-}
